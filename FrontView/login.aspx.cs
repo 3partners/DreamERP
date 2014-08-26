e08fef34097939d2ddd864be53384a8a9ad459f8
3partners <ssn3partners@gmail.com>
2014-08-26 22:12:48 +0530

    Made changes to set the project
    Made changes in web.config to accomodate login

---
+++ FrontView/login.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+
+public partial class FrontView_login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        string password = WebConfigurationManager.AppSettings["superadmin"];
+        if (txtPassword.ToString().Trim() == password && txtUser.ToString().Trim() == "superadmin")
+            Response.Redirect("Dashboard.aspx");
+        
+    }
+}
